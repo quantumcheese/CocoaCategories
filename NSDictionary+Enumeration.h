0e884de248a79c1ffa707fa9004c8230f23f5367
Richard Brown <rabrown@quantumcheese.com>
2013-08-15 14:35:02 -0400

    Adding Dictionary enumeration to keep the predicate block separate from the processing block.

---
+++ NSDictionary+Enumeration.h
@@ -0,0 +1,22 @@
+// MARK: Interface
+
+@interface NSDictionary (Enumeration)
+- (void)enumerateKeysAndObjectsPassingTest:(BOOL (^)(id key, id obj, BOOL *stop))predicate
+                                usingBlock:(void (^)(id key, id obj, BOOL *stop))block;
+@end
+
+// MARK: Implementation
+
+@implementation NSDictionary (Enumeration)
+
+- (void)enumerateKeysAndObjectsPassingTest:(BOOL (^)(id key, id obj, BOOL *stop))predicate
+                                usingBlock:(void (^)(id key, id obj, BOOL *stop))block
+{
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        if (predicate(key, obj, stop) && !stop) {
+            block(key, obj, stop);
+        }
+    }];
+}
+
+@end
